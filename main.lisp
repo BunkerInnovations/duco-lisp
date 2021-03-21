3b57704ad9d82507b41c60259e27f3f7a6af9594
Abb1x <abb1x@protonmail.com>
2021-03-20 20:34:18 -0400

    Added username getting

--- main.lisp
+++ main.lisp
@@ -1,1 +1,17 @@
-(format t "Hello! Welcome to the ~c[33mDuinocoin~c[0m lisp miner! ~%" #\ESC #\ESC)
+(defvar username)
+
+(defun get-username (prompt) ; utilities function to get username
+  (clear-input)                     
+  (write-string prompt)             
+  (finish-output)                   
+  (setf username (read-line)))           
+
+
+(defun init()
+  (get-username "Enter username: ")
+  (format t "Hello ~c[32m~a~c[0m! Welcome to the ~c[33mDuinocoin~c[0m lisp miner! ~%" #\ESC username #\ESC #\ESC #\ESC)
+  )
+
+(init)
+
+
